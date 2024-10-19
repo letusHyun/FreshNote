3258823218a243cf7c88f2a751be8e4212bf4010
seokhyun <letushyun@gmail.com>
2024-10-19 16:07:40 +0900

    Initial Commit

---
+++ FreshNote/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FreshNote
+//
+//  Created by SeokHyun on 10/19/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
---
+++ FreshNoteTests/FreshNoteTests.swift
@@ -0,0 +1,17 @@
+//
+//  FreshNoteTests.swift
+//  FreshNoteTests
+//
+//  Created by SeokHyun on 10/19/24.
+//
+
+import Testing
+@testable import FreshNote
+
+struct FreshNoteTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
