f17fed16125aec57b6bf81068fa8148b6087510e
seokhyun <letushyun@gmail.com>
2024-11-19 17:08:53 +0900

    [Feat] 완료버튼이 달린 toolbar 구현

---
+++ FreshNote/Common/View/BaseKeyboardToolbar.swift
@@ -0,0 +1,57 @@
+//
+//  BaseKeyboardToolbar.swift
+//  FreshNote
+//
+//  Created by SeokHyun on 11/19/24.
+//
+
+import Combine
+import UIKit
+
+class BaseKeyboardToolbar: UIToolbar {
+  // MARK: - Properties
+  var tapPublisher: AnyPublisher<Void, Never> { self.tapSubject.eraseToAnyPublisher() }
+  
+  private let tapSubject: PassthroughSubject<Void, Never> = .init()
+  
+  private let doneButton: UIButton = {
+    let btn = UIButton()
+    btn.setTitle("완료", for: .normal)
+    btn.setTitleColor(UIColor(fnColor: .gray3), for: .normal)
+    return btn
+  }()
+
+  private var subscriptions = Set<AnyCancellable>()
+  
+  // MARK: - LifeCycle
+  init() {
+    super.init(frame: CGRect(
+      x: .zero,
+      y: .zero,
+      width: UIScreen.main.bounds.width,
+      height: 50
+    ))
+    self.setupToolbar()
+    self.bind()
+  }
+  
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  // MARK: - Private Helpers
+  private func bind() {
+    self.doneButton.publisher(for: .touchUpInside)
+      .receive(on: RunLoop.main)
+      .sink { [weak self] _ in
+        self?.tapSubject.send()
+      }
+      .store(in: &self.subscriptions)
+  }
+  
+  private func setupToolbar() {
+    let emptySpace = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
+    let barButtonItem = UIBarButtonItem(customView: self.doneButton)
+    self.items = [emptySpace, barButtonItem]
+  }
+}
