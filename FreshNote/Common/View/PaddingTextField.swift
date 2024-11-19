eef2277c1bf766155a67db31533b73af7f6ddaf0
seokhyun <letushyun@gmail.com>
2024-11-19 17:07:51 +0900

    [Feat] 키보드의 완료 누르면 down 처리

--- FreshNote/Common/View/PaddingTextField.swift
+++ FreshNote/Common/View/PaddingTextField.swift
@@ -5,24 +5,43 @@
 //  Created by SeokHyun on 11/1/24.
 //
 
+import Combine
 import UIKit
 
 /// 텍스트 left padding을 제공해주는 객체입니다.
 class PaddingTextField: UITextField {
+  // MARK: - Properties
+  private let keyboardToolbar = BaseKeyboardToolbar()
+  
+  private var subscriptions = Set<AnyCancellable>()
+  
   // MARK: - LifeCycle
   init(leftPadding: CGFloat = 25) {
     super.init(frame: .zero)
-    setPadding(leftPadding: leftPadding)
+    self.setPadding(leftPadding: leftPadding)
+    self.setupToolbar()
+    self.bind()
   }
   
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-}
-
-// MARK: - Private Helpers
-private extension PaddingTextField {
-  func setPadding(leftPadding: CGFloat) {
+  
+  // MARK: - Private Helpers
+  private func bind() {
+    self.keyboardToolbar.tapPublisher
+      .receive(on: RunLoop.main)
+      .sink { [weak self] in
+        self?.endEditing(true)
+      }
+      .store(in: &self.subscriptions)
+  }
+  
+  private func setupToolbar() {
+    self.inputAccessoryView = self.keyboardToolbar
+  }
+  
+  private func setPadding(leftPadding: CGFloat) {
     self.leftView  = UIView(frame: CGRect(x: .zero, y: .zero, width: leftPadding, height: frame.height))
     self.leftViewMode = .always
   }
