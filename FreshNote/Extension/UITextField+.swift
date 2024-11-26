1aad204bc9d9177e10b0df5ab32d568fb552f4b0
seokhyun <letushyun@gmail.com>
2024-11-26 23:14:24 +0900

    [Feat] textField에 textDidChangePublisher 추가

--- FreshNote/Extension/UITextField+.swift
+++ FreshNote/Extension/UITextField+.swift
@@ -5,6 +5,7 @@
 //  Created by SeokHyun on 11/5/24.
 //
 
+import Combine
 import UIKit
 
 extension UITextField {
@@ -17,5 +18,12 @@ extension UITextField {
         .font: font
       ].compactMapValues { $0 }
     )
+  }
+  
+  var textDidChangedPublisher: AnyPublisher<String, Never> {
+    NotificationCenter.default.publisher(for: UITextField.textDidChangeNotification, object: self)
+      .compactMap { ($0.object as? UITextField)?.text }
+      .receive(on: DispatchQueue.main)
+      .eraseToAnyPublisher()
   }
 }
