161d30e417b836078a91c12f658bf5ab377cd8e9
seokhyun <letushyun@gmail.com>
2024-11-28 01:52:25 +0900

    [Feat] Encodable의 toDictionary 추가

---
+++ FreshNote/Extension/Encodable+.swift
@@ -0,0 +1,16 @@
+//
+//  Encodable+.swift
+//  FreshNote
+//
+//  Created by SeokHyun on 11/28/24.
+//
+
+import Foundation
+
+//private extension Encodable {
+//  func toDictionary() throws -> [String: Any]? {
+//    let data = try JSONEncoder().encode(self)
+//    let jsonData = try JSONSerialization.jsonObject(with: data)
+//    return jsonData as? [String : Any]
+//  }
+//}
